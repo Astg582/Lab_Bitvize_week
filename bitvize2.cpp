ed32bea8db8fe7e99416fe9c8ca34f6c664cc0e4
Astg582 <109224508+Astg582@users.noreply.github.com>
2023-12-14 14:41:14 +0400

    Add files via upload

--- bitvize2.cpp
+++ bitvize2.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 
-void swap1(int& n, int i, int j);
-int sum1(int a, int b);
-void revers(int& n, int i, int j);
-int sum2(int a, int b);
-int sub1(int a, int b);
+void swap1(int&, int, int);
+int sum1(int, int);
+void revers(int&, int, int);
+int sum2(int, int);
+int sub1(int, int);
 int sub2(int, int);
+int mull(int, int);
+int mull_rec(int a, int b);
 
 int main(){
 	int n = 0;
 	int j = 0;
 	std::cin >> n >> j;
 	//revers(n, i, j);
-	std::cout << sub2(n, j) << std::endl;
+	std::cout << mull_rec(n, j) << std::endl;
 }
 
 int sum1(int a, int b){
@@ -58,8 +60,6 @@ void revers(int& n, int i, int j){
 }
 
 
-
-
 void swap1(int& n, int i, int j){
 	if(!(i < 0) && !(j < 0) && (i < 32) && (j < 32)){ 
 		if(bool(n & (1 << i)) == bool(n & (1 << j))){
@@ -69,3 +69,36 @@ void swap1(int& n, int i, int j){
 		n ^= (1 << j);
 	}
 }
+
+int mull(int a, int b){
+	int rec = 0;
+	while(b){
+		if(b & 1){
+			rec = sum1(rec, a);
+		}
+		a <<= 1;
+		b >>= 1;
+		
+	}
+	return rec;
+}
+
+
+int mull_rec(int a, int b){
+	if(b == 0){
+		return 0;
+	}
+	if(b & 1){
+	return sum1(mull_rec(a << 1, b >> 1), a);
+	}
+	return mull_rec(a << 1, b >> 1);
+}
+
+
+
+
+
+
+
+
+
